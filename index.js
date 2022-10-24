ebf7265dcada5ea288280fb6e531e6bc740c03d0
Anton Popov <a.popov.d@gmail.com>
2022-10-24 19:47:46 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,57 @@
+async function handleFiles(files) {
+  const file = files[0];
+  const div = document.querySelector('.block__image-div');
+  const loader = document.querySelector('.block__loader');
+  let img = document.querySelector('.block__image-div-obj');
+
+  if (!img) {
+    img = document.createElement("img");
+    img.classList.add("block__image-div-obj");
+    img.file = file;
+    div.append(img);
+  }
+  div.style.display = 'none';
+  loader.style.display = 'block';
+
+  img.addEventListener('load', () => {
+    loader.style.display = 'none';
+    div.style.display = 'block';
+  });
+
+  await sendFile(img, file);
+}
+
+async function sendFile(img, file) {
+  const fd = new FormData();
+  fd.append('uploadFile', file);
+  let newFile = undefined
+  await fetch('http://45.130.151.203:3000/', {
+    method: 'POST',
+    body: fd
+  })
+  .then((resp) => {
+    return resp.blob();
+  })
+  .then((blob) => {
+    newFile = blob;
+  });
+  const a = document.querySelector('.download');
+  const url = window.URL.createObjectURL(newFile);
+  a.href = url;
+  a.click();
+  window.URL.revokeObjectURL(url);
+  loadImg(newFile, img);
+}
+
+function loadImg(file, div) {
+  const reader = new FileReader();
+  reader.onload = (
+    function(aImg) {
+      return function(e) {
+        aImg.src = e.target.result;
+      };
+    }
+  )(div);
+
+  reader.readAsDataURL(file);
+}
